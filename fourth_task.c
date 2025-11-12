b5c74060517c7e19e1c8a2bbb9985c79ccae0e01
Danca402 <orbantamasdaniel@gmail.com>
2025-11-12 16:27:25 +0000

    fourth_task.c

--- fourth_task.c
+++ fourth_task.c
@@ -7,7 +7,32 @@ Create in main an array of five, and another one of ten integers (initialized). 
 
 #include <stdio.h>
 
+
+
+void forwards(int array[], int len, int id){
+  if(len==id) return;
+  printf("%d ",array[id]);
+  forwards(array,len,id+1);
+}
+
+
+void backwards(int array[], int len, int id){
+  if(len==id) return;
+  backwards(array,len,id+1);
+  printf("%d ",array[id]);
+}
+
 int main(){
+  int array5[]={1,2,3,4,6};
+  int array10[]={1,2,3,4,6,43,54,54,54,321};
+
+  forwards(array5,5,0);
+  printf("\n");
+  backwards(array5,5,0);
+  printf("\n");
+  forwards(array10,10,0);
+  printf("\n");
+  backwards(array10,10,0);
 
 
   return 0;
